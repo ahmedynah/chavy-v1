66c490eba7e806b98299c4723d89fb25f18baf4e
ahmedynah <40689146+ahmedynah@users.noreply.github.com>
2022-06-20 19:48:39 +0200

    added arabic labels to accoridion

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -20,26 +20,26 @@ import { ChevronDownIcon } from '@heroicons/react/solid'
 import { MdOutlineShoppingCart } from "react-icons/md";
 const solutions = [
   {
-    name: 'Analytics',
+    name: 'الرئيسية',
     description: 'Get a better understanding of where your traffic is coming from.',
     href: '#',
     icon: ChartBarIcon,
   },
   {
-    name: 'Engagement',
+    name: 'المدونة',
     description: 'Speak directly to your customers in a more meaningful way.',
     href: '#',
     icon: CursorClickIcon,
   },
-  { name: 'Security', description: "Your customers' data will be safe and secure.", href: '#', icon: ShieldCheckIcon },
+  { name: 'تسوق', description: "Your customers' data will be safe and secure.", href: '#', icon: ShieldCheckIcon },
   {
-    name: 'Integrations',
+    name: 'من نحن',
     description: "Connect with third-party tools that you're already using.",
     href: '#',
     icon: ViewGridIcon,
   },
   {
-    name: 'Automations',
+    name: 'اتصل بنا',
     description: 'Build strategic funnels that will drive your customers to convert',
     href: '#',
     icon: RefreshIcon,
